d3394d8235fb65f549348629f4bad6d13ce6307f
mukesh jamod <mukeshjamod28@gmail.com>
2023-03-14 18:53:27 +0530

    added code

--- my-userinput-app/src/components/BasicForm.js
+++ my-userinput-app/src/components/BasicForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
-const BasicForm = () => {
-  return (
-    <div>BasicForm</div>
-  )
-}
-
-export default BasicForm
+const BasicForm = (props) => {
+    return (
+      <form>
+        <div className='control-group'>
+          <div className='form-control'>
+            <label htmlFor='name'>First Name</label>
+            <input type='text' id='name' />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='name'>Last Name</label>
+            <input type='text' id='name' />
+          </div>
+        </div>
+        <div className='form-control'>
+          <label htmlFor='name'>E-Mail Address</label>
+          <input type='text' id='name' />
+        </div>
+        <div className='form-actions'>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  };
+  
+  export default BasicForm;
--- my-userinput-app/src/components/SimpleInput.js
+++ my-userinput-app/src/components/SimpleInput.js
@@ -1,17 +1,93 @@
-import React from 'react'
+import { useState} from 'react';
 
 const SimpleInput = (props) => {
-  return (
-   <form>
-    <div className='form-control'>
-        <label htmlfor='name'>Your Name </label>
-        <input type='text' id='name'></input>
-    </div>
-    <div className='form-actions'>
-    <button>Submit</button>
-    </div>
-   </form>
-  )
+  //const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  //const [enteredNameIsValid, setEnteredNameIsValid]=useState(false);
+  const [enteredNameTouched,setEnteredNameTouched] = useState(false)
+  //const [formIsValid,setFormIsValid] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+  
+  let formIsValid = false;
+ 
+if(enteredNameIsValid)
+{
+    formIsValid(true);
 }
+  
+//    useEffect(() => {
+//      if(enteredNameIsValid){
+//         console.log('input is valid');
+//      }
+//   }, [enteredNameIsValid]);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+
+    // if(event.target.value.trim() !== ''){
+    //     setEnteredNameIsValid(true);
+       
+    // }
+  };
+   
+  const nameInputBlurHandler = event =>{
+  setEnteredNameTouched(true);
+
+//   if(enteredName.trim() === ''){
+//     setEnteredNameIsValid(false);
+//     return;
+// }
+  };
+
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    //console.log(enteredName);
+    if(!enteredNameIsValid){
+        return;
+    }
+ console.log(enteredName);
+//     if(enteredName.trim() === ''){
+//         setEnteredNameIsValid(false);
+//         return;
+//     }
+//    setEnteredNameIsValid(true);
+
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredName('');
+    setEnteredNameTouched(false); 
+  };
+
+//   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+   const nameInputClasses = enteredNameIsValid ? 'form-control invalid': 'form-control';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input
+       
+          type='text'
+          id='name'
+          onBlur={nameInputBlurHandler}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+      </div>
+     {nameInputIsValid && <p className='error-text'>name must not be empty</p>}
+      <div className='form-actions'>
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
 
 export default SimpleInput;
